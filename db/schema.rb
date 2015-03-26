268b263e261d28cb854bd2efd4290fa62d045692
Mark Lohr <markuslohr@gmail.com>
2015-03-26 11:22:18 -0400

    reset db and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317191516) do
+ActiveRecord::Schema.define(version: 20150323180838) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -26,5 +26,26 @@ ActiveRecord::Schema.define(version: 20150317191516) do
     t.float    "latitude",   limit: 24
     t.float    "longitude",  limit: 24
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.string   "profile_name",           limit: 255
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
